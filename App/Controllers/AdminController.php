1fbaea83aca250be8c10cd0d88d11f9c11e5748b
LJAMINE <aminelmgrmj@gmail.com>
2025-02-12 15:41:41 +0100

    add function of switchStatus sur controller et UserRempository

--- App/Controllers/AdminController.php
+++ App/Controllers/AdminController.php
@@ -1,65 +1,116 @@
 <?php
+
 namespace App\Controllers;
+
 use App\Core\Session;
+use App\Repositories\UserRepository;
 
+class AdminController
+{
 
-class AdminController {
+    private $userRepository;
 
-   
-      public function renderLayout($page){
-        require_once  dirname(__DIR__).'\Views\Dashboard\Admin\layouts\header.php';  
-        require_once dirname(__DIR__).'\Views\Dashboard\Admin\layouts\sidebar.php';  
-        require_once dirname(__DIR__).'\Views\Dashboard\Admin\layouts\section.php';  
-        require_once dirname(__DIR__).'\Views\Dashboard\Admin\layouts\navbar.php';  
-        require_once dirname(__DIR__).'\Views\Dashboard\Admin\layouts\homeContent.php'; 
-         
+    public function __construct()
+    {
+        $this->userRepository = new UserRepository();
+    }
+
+    public function renderLayout($page)
+    {
+        require_once  dirname(__DIR__) . '\Views\Dashboard\Admin\layouts\header.php';
+        require_once dirname(__DIR__) . '\Views\Dashboard\Admin\layouts\sidebar.php';
+        require_once dirname(__DIR__) . '\Views\Dashboard\Admin\layouts\section.php';
+        require_once dirname(__DIR__) . '\Views\Dashboard\Admin\layouts\navbar.php';
+        require_once dirname(__DIR__) . '\Views\Dashboard\Admin\layouts\homeContent.php';
+
         //todo include la page specifique selon le nom  de fichier qui est doit correspondant avec nom de page
-            require_once dirname(__DIR__,1).'\Views\Dashboard\Admin\pages\\' . $page . '.php';
+        require_once dirname(__DIR__, 1) . '\Views\Dashboard\Admin\pages\\' . $page . '.php';
 
-        require_once dirname(__DIR__).'\Views\Dashboard\Admin\layouts\endHomeContent.php';  
-        require_once dirname(__DIR__).'\Views\Dashboard\Admin\layouts\endSection.php';  
-        require_once dirname(__DIR__).'\Views\Dashboard\Admin\layouts\footer.php';  
-    }
-
-    public function statistiquesView() {
-        $this->renderLayout('statistiques'); 
+        require_once dirname(__DIR__) . '\Views\Dashboard\Admin\layouts\endHomeContent.php';
+        require_once dirname(__DIR__) . '\Views\Dashboard\Admin\layouts\endSection.php';
+        require_once dirname(__DIR__) . '\Views\Dashboard\Admin\layouts\footer.php';
     }
 
-    public function organisateursView() {
-        $this->renderLayout('organisateurs'); 
+    public function statistiquesView()
+    {
+        $this->renderLayout('statistiques');
     }
 
-    public function participantsView() {
-        $this->renderLayout('participants'); 
-    }
-    
-    public function eventsView() {
-        $this->renderLayout('events'); 
+    public function organisateursView()
+    {
+        $this->renderLayout('organisateurs');
     }
 
-    public function categoriesView() {
-        $this->renderLayout('categories'); 
+    public function participantsView()
+    {
+        $this->renderLayout('participants');
     }
-    
-    public function editCategorieView() {
-        $this->renderLayout('editCategorie'); 
+
+    public function eventsView()
+    {
+        $this->renderLayout('events');
     }
-    
-    public function tagsView() {
-        $this->renderLayout('tags'); 
+
+    public function categoriesView()
+    {
+        $this->renderLayout('categories');
     }
-    
-    public function editTagView() {
-        $this->renderLayout('editTag'); 
+
+    public function editCategorieView()
+    {
+        $this->renderLayout('editCategorie');
     }
-    
-  
-    
-    public function logoutView() {
-        $this->renderLayout('logout'); 
+
+    public function tagsView()
+    {
+        $this->renderLayout('tags');
     }
-    
-    
-   
-  
+
+    public function editTagView()
+    {
+        $this->renderLayout('editTag');
+    }
+
+
+
+    public function logoutView()
+    {
+        $this->renderLayout('logout');
+    }
+
+    //-------------amine : displayOrganisateur()------------
+    public function displayOrganisateur()
+    {
+        return $this->userRepository->display();
+    }
+
+    public function rederRow($organisateur)
+    {
+
+        return "<tr>
+        <td> " . $organisateur->getPrenom() . "</td>
+        <td> " . $organisateur->getNom() . "</td>
+        <td> " . $organisateur->getEmail() . "</td>
+        <td> " . $organisateur->getStatus() . "</td>
+        <td>
+        <a href='admin/updateStatusUser/". $organisateur->getID() ."'>switch</a>
+        </td>
+    </tr>";
+    }
+
+    public function updateStatusUser($id)
+    {
+
+        // echo "methode update";
+        if (isset($_GET["id"])) {
+            $id = $_GET["id"];
+
+            $this->userRepository->updateStatus("users","users_id",$id);
+        }
+    }
+
+    //-------------amine : displayOrganisateur()------------
+
+
+
 }
