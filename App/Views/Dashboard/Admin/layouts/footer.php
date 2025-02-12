58dfa0b98d40b694da13b662fbf3da87f3c79306
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2025-02-12 10:03:28 +0100

    Créer un fichier admin.js pour admin et organisateur.js pour organisateur

--- App/Views/Dashboard/Admin/layouts/footer.php
+++ App/Views/Dashboard/Admin/layouts/footer.php
@@ -1,5 +1,5 @@
 <!-- footer -->
-<script src="/public/assets/js/Dashboard/dashboard.js"> </script>
+<script src="/public/assets/js/Dashboard/Admin/admin.js"> </script>
 
 <!-- bootstrap -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
---
+++ public/assets/js/Dashboard/Organisateur/organisateur.js
@@ -0,0 +1,44 @@
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+
+sidebarBtn.onclick = function () {
+  sidebar.classList.toggle("active");
+  if (sidebar.classList.contains("active")) {
+    sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+  } else sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const currentPage = window.location.href;
+
+  const links = document.querySelectorAll(".sidebar a");
+
+  links.forEach((link) => {
+    if (currentPage.includes(link.getAttribute("href"))) {
+      link.classList.add("active");
+    }
+  });
+});
+
+// Masquer l'alerte après 3 secondes (3000 millisecondes)
+setTimeout(function () {
+  var alertElement = document.querySelector(".alert");
+  if (alertElement) {
+    alertElement.style.display = "none";
+  }
+}, 2000);
+
+// spinner
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("loader").style.display = "none";
+});
+
+document.querySelectorAll("a").forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    document.getElementById("loader").style.display = "flex";
+
+    window.onload = function () {
+      document.getElementById("loader").style.display = "none";
+    };
+  });
+});
