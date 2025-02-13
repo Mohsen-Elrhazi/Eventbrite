d774e48d2f054debc92e19353dd1add0016522bf
mohamedamid <amid.mohamed2024@gmail.com>
2025-02-13 10:49:46 +0100

    create code ajax add et delete

---
+++ App/Controllers/EventController.php
@@ -0,0 +1,65 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\Event;
+use App\Repositories\EventRepository;
+
+class EventController
+{
+    private $eventRepository;
+
+    public function __construct()
+    {
+        $this->eventRepository = new EventRepository();
+    }
+
+    public function indexView()
+    {
+        echo json_encode($this->eventRepository->display());
+    }
+
+    public function store()
+    {
+        $data = $_POST;
+        $event = new Event(
+            $data['titre'],
+            $data['description'],
+            $data['event_date'],
+            $data['heure_debut'],
+            $data['heure_fin'],
+            $data['prix'],
+            $data['image'],
+            $data['content_url'],
+            $data['category_id']
+        );
+        echo $this->eventRepository->save($event);
+    }
+
+    public function updateView()
+    {
+        $data = $_POST;
+        $event = new Event(
+            $data['titre'],
+            $data['description'],
+            $data['event_date'],
+            $data['heure_debut'],
+            $data['heure_fin'],
+            $data['prix'],
+            $data['image'],
+            $data['content_url'],
+            $data['category_id'],
+            $data['event_id']
+        );
+        echo $this->eventRepository->edit($event);
+    }
+
+    public function destroy($id)
+    {
+        echo $this->eventRepository->delete($id);
+    }
+
+    public function showView($id)
+    {
+        echo json_encode($this->eventRepository->findByID($id));
+    }
+}
