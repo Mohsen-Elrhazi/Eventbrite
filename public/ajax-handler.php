9532d96a7ae03c6eaeba656ffe1e7e2b68c475a5
mohamedamid <amid.mohamed2024@gmail.com>
2025-02-13 11:39:30 +0100

    sweetAlert

--- public/ajax-handler.php
+++ public/ajax-handler.php
@@ -1,22 +1,22 @@
 <?php
-require_once '../App/Controllers/EventController.php';
+// require_once '../App/Controllers/EventController.php';
 
-$controller = new EventController();
+// $controller = new EventController();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if ($_POST['action'] === 'create') {
-        $controller->store($_POST);
-    } elseif ($_POST['action'] === 'update') {
-        $controller->update($_POST);
-    } elseif ($_POST['action'] === 'delete') {
-        $controller->destroy($_POST['id']);
-    }
-} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if ($_GET['action'] === 'load') {
-        $events = $controller->index();
-        echo json_encode(['status' => 'success', 'events' => $events]);
-    } elseif ($_GET['action'] === 'loadEvent' && isset($_GET['id'])) {
-        $event = $controller->show($_GET['id']);
-        echo json_encode(['status' => 'success', 'event' => $event]);
-    }
-}
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//     if ($_POST['action'] === 'create') {
+//         $controller->store($_POST);
+//     } elseif ($_POST['action'] === 'update') {
+//         $controller->update($_POST);
+//     } elseif ($_POST['action'] === 'delete') {
+//         $controller->destroy($_POST['id']);
+//     }
+// } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+//     if ($_GET['action'] === 'load') {
+//         $events = $controller->index();
+//         echo json_encode(['status' => 'success', 'events' => $events]);
+//     } elseif ($_GET['action'] === 'loadEvent' && isset($_GET['id'])) {
+//         $event = $controller->show($_GET['id']);
+//         echo json_encode(['status' => 'success', 'event' => $event]);
+//     }
+// }
